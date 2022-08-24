20895a6d1808f1c32e56923e6111e8f07009e5ec
Sudheer Mocharla <msudheerhyd@gmail.com>
2022-08-24 22:18:31 +0530

    24-Aug-22:Wed, Added Spring-boot-tutorial2

---
+++ Spring-boot-tutorial2/src/test/java/com/dailycodebuffer/Springboottutorial2/SpringBootTutorial2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.Springboottutorial2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorial2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
