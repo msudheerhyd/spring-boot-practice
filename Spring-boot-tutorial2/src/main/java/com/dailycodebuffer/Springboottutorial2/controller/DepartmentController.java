1c5ddf3dd4bb0bf216fed6a111982ae9c9c3e7c5
Sudheer Mocharla <msudheerhyd@gmail.com>
2022-08-27 22:35:23 +0530

    27-Aug-22:Sat, Added Spring-boot-tutorial2

--- Spring-boot-tutorial2/src/main/java/com/dailycodebuffer/Springboottutorial2/controller/DepartmentController.java
+++ Spring-boot-tutorial2/src/main/java/com/dailycodebuffer/Springboottutorial2/controller/DepartmentController.java
@@ -3,9 +3,10 @@ package com.dailycodebuffer.Springboottutorial2.controller;
 import com.dailycodebuffer.Springboottutorial2.entity.Department;
 import com.dailycodebuffer.Springboottutorial2.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -14,7 +15,34 @@ public class DepartmentController {
     private DepartmentService departmentService;
 
     @PostMapping("/departments")
-    public Department saveDepartment(@RequestBody Department department) {
+    public Department saveDepartment(@Valid @RequestBody Department department) {
         return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/departments")
+    public List<Department> fetchDepartmentList() {
+        return departmentService.fetchDepartmentList();
+    }
+
+    @GetMapping("/departments/{id}")
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId) {
+        return departmentService.fetchDepartmentById(departmentId);
+    }
+
+    @DeleteMapping("/departments/{id}")
+    public String deleteDepartmentById(@PathVariable("id") Long departmentId) {
+        departmentService.deleteDepartmentById(departmentId);
+        return "Department deleted Successfully";
+    }
+
+    @PutMapping("/departments/{id}")
+    public Department updateDepartment(@PathVariable("id") Long departmentId,
+                                       @RequestBody Department department) {
+        return departmentService.updateDepartment(departmentId,department);
+    }
+
+    @GetMapping("/departments/name/{name}")
+    public Department fetchDepartmentByName(@PathVariable("name") String departmentName) {
+        return departmentService.fetchDepartmentByName(departmentName);
     }
 }
--- Spring-boot-tutorial2/src/main/java/com/dailycodebuffer/Springboottutorial2/service/DepartmentService.java
+++ Spring-boot-tutorial2/src/main/java/com/dailycodebuffer/Springboottutorial2/service/DepartmentService.java
@@ -2,6 +2,18 @@ package com.dailycodebuffer.Springboottutorial2.service;
 
 import com.dailycodebuffer.Springboottutorial2.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
     public Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
+
+    public Department fetchDepartmentById(Long departmentId);
+
+    public void deleteDepartmentById(Long departmentId);
+
+    public Department updateDepartment(Long departmentId, Department department);
+
+    Department fetchDepartmentByName(String departmentName);
 }
