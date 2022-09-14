a178358f2cc27bf00eccd17edc99989e425a2a4f
Sudheer Mocharla <msudheerhyd@gmail.com>
2022-09-14 22:09:06 +0530

    14-Sep-22:Wed, Added spring-boot-mongodb

--- springboot-mongodb/src/main/java/com/dailycodebuffer/springbootmongodb/controller/PersonController.java
+++ springboot-mongodb/src/main/java/com/dailycodebuffer/springbootmongodb/controller/PersonController.java
@@ -2,7 +2,11 @@ package com.dailycodebuffer.springbootmongodb.controller;
 
 import com.dailycodebuffer.springbootmongodb.collection.Person;
 import com.dailycodebuffer.springbootmongodb.service.PersonService;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -33,5 +37,29 @@ public class PersonController {
     public List<Person> getByPersonAge(@RequestParam Integer minAge,
                                        @RequestParam Integer maxAge) {
         return personService.getByPersonAge(minAge,maxAge);
+    }
+
+    @GetMapping("/search")
+    public Page<Person> searchPerson(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) Integer minAge,
+            @RequestParam(required = false) Integer maxAge,
+            @RequestParam(required = false) String city,
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "5") Integer size
+    ) {
+        Pageable pageable
+                = PageRequest.of(page,size);
+        return personService.search(name,minAge,maxAge,city,pageable);
+    }
+
+    @GetMapping("/oldestPerson")
+    public List<Document> getOldestPerson() {
+        return personService.getOldestPersonByCity();
+    }
+
+    @GetMapping("/populationByCity")
+    public List<Document> getPopulationByCity() {
+        return personService.getPopulationByCity();
     }
 }
