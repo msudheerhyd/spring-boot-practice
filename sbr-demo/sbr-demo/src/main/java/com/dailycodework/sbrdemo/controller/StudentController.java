afa090920051a0a6e6414b86f40db70106c73020
msudheerhyd <msudheerhyd@gmail.com>
2024-05-24 19:03:05 -0700

    Added sbr-demo and LeetCode Problem

--- sbr-demo/sbr-demo/src/main/java/com/dailycodework/sbrdemo/controller/StudentController.java
+++ sbr-demo/sbr-demo/src/main/java/com/dailycodework/sbrdemo/controller/StudentController.java
@@ -34,4 +34,9 @@ public class StudentController {
     public void deleteStudent(@PathVariable Long id) {
         studentService.deleteStudent(id);
     }
+
+    @GetMapping("/student/{id}")
+    public Student getStudentById(@PathVariable long id) {
+        return studentService.getStudentById(id);
+    }
 }
