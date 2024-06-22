c8a229d71c71561c6c448605464db6413d650fd0
msudheerhyd <msudheerhyd@gmail.com>
2024-06-22 12:23:56 -0700

    Added sbr-demo and LeetCode Problem

---
+++ LeetCodePractice/src/blind75/UglyNumber.java
@@ -0,0 +1,19 @@
+package blind75;
+
+public class UglyNumber {
+
+    public static boolean uglyNumber(int n) {
+        if(n <= 0) return false;
+
+        while(n%5==0) n/=5;
+        while(n%3==0) n/=3;
+        while(n%2==0) n/=2;
+
+        return n==1;
+    }
+
+    public static void main(String[] args) {
+        int n = 15;
+        System.out.println(uglyNumber(n));
+    }
+}
--- microservices-new/order-service/src/main/java/com/programmingtechie/order_service/service/OrderService.java
+++ microservices-new/order-service/src/main/java/com/programmingtechie/order_service/service/OrderService.java
@@ -6,8 +6,10 @@ import com.programmingtechie.order_service.model.Order;
 import com.programmingtechie.order_service.model.OrderLineItems;
 import com.programmingtechie.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
@@ -17,7 +19,9 @@ import java.util.UUID;
 @Transactional 
 public class OrderService {
 
+    @Autowired
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -30,7 +34,24 @@ public class OrderService {
 
         order.setOrderLineItems(orderLineItems);
 
-        orderRepository.save(order);
+        List<String> skuCodes = order.getOrderLineItems().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+
+        //Call Inventory Service, and place order if the product is in stock
+        Boolean result  = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .block();
+
+        if(result) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
